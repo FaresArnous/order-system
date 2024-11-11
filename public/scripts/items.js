c187339dfd14128642dfbd62feaaa9c17b0968cd
fares Arnous <Faris.Arnous@hotmail.com>
2024-11-11 16:38:20 +0300

    exclude Water from the radio options

--- public/scripts/items.js
+++ public/scripts/items.js
@@ -22,13 +22,18 @@ function createMenuItem(item) {
       <button class="decrease" data-item="${item.name}">-</button>
       <input class="ss" type="text" id="${formattedId}-quantity" value="0" />
       <button class="increase" data-item="${item.name}">+</button>
+      ${
+        item.name !== "Water"
+          ? `
       <div class="select">
         <input type="radio" id="${formattedId}-sugar" name="${formattedId}-sugar" class="sugar" />
         <label for="${formattedId}-sugar">With Sugar</label>
 
         <input type="radio" id="${formattedId}-no-sugar" name="${formattedId}-sugar" class="no-sugar" />
         <label for="${formattedId}-no-sugar">Without Sugar</label>
-      </div>
+      </div>`
+          : ""
+      }
     </div>
   `;
 
