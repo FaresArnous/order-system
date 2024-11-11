632099fdf3ad241b371131aa1a0ed104fec4f99f
fares Arnous <Faris.Arnous@hotmail.com>
2024-11-11 13:31:12 +0300

    added clear btn to each order

--- public/scripts/order.js
+++ public/scripts/order.js
@@ -3,13 +3,12 @@ const socket = io(); // Connect to the server
 socket.on("order-update", (order) => {
   const orderSummary = document.getElementById("order-summary");
 
-  // Create a container for the new order
   const newOrderContainer = document.createElement("div");
   newOrderContainer.classList.add("new-order");
 
-  // Add a heading or separator for each new order
   const orderHeading = document.createElement("h3");
   orderHeading.textContent = "New Order";
+
   newOrderContainer.appendChild(orderHeading);
 
   // Iterate through the order items and display each one
@@ -29,10 +28,21 @@ socket.on("order-update", (order) => {
     orderItem.appendChild(itemQuantity);
 
     newOrderContainer.appendChild(orderItem);
-    const sepratorLine = document.createElement("hr");
-    sepratorLine.id = "line";
-    newOrderContainer.appendChild(sepratorLine);
   }
+
+  const clearButton = document.createElement("button");
+  clearButton.classList.add("clear-btn");
+  clearButton.textContent = "Clear Order";
+
+  clearButton.addEventListener("click", () => {
+    orderSummary.removeChild(newOrderContainer);
+  });
+
+  newOrderContainer.appendChild(clearButton);
+
+  const separatorLine = document.createElement("hr");
+  separatorLine.id = "line";
+  newOrderContainer.appendChild(separatorLine);
 
   orderSummary.appendChild(newOrderContainer);
 });
